633b870fb5a660fecec1bab34aee87548508c3a7
SupuniRatnayake <supuniratnayake@gmail.com>
2026-09-18 19:56:23 +0530

    Initial commit

---
+++ Smart-Hospital-Resource-Allocation-System/Smart-Hospital-Resource-Allocation-System/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Smart-Hospital-Resource-Allocation-System
+//
+//  Created by Supuni Ratnayake on 2026-09-18.
+//
+
+#include <stdio.h>
+
+int main (void) {
+    
+    printf("Smart Hospital & Resource Allocation System\n");
+    
+    return 0;
+}
