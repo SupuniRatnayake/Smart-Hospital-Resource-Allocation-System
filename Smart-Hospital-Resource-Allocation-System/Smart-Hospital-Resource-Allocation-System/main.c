2c8f9c17bfaea0e8f0f8b6498cf561938887b436
SupuniRatnayake <supuniratnayake@gmail.com>
2026-09-18 23:33:07 +0530

    Add doctor specialties data

--- Smart-Hospital-Resource-Allocation-System/Smart-Hospital-Resource-Allocation-System/main.c
+++ Smart-Hospital-Resource-Allocation-System/Smart-Hospital-Resource-Allocation-System/main.c
@@ -7,9 +7,40 @@
 
 #include <stdio.h>
 
+#define SPECIALTIES_NUM 4
+
+const char specialtyName[SPECIALTIES_NUM][30] = {
+    "General Practice (OPD)",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
+};
+
+const double consultationFee[SPECIALTIES_NUM] = {
+    1500.00,
+    2500.00,
+    4500.00,
+    5000.00
+};
+
+const int consultationTime[SPECIALTIES_NUM] = {
+    15,
+    20,
+    30,
+    30
+};
+
+const int dailyPatientCap[SPECIALTIES_NUM] = {
+    30,
+    20,
+    12,
+    10
+};
+
 int main (void) {
     
     printf("Smart Hospital & Resource Allocation System\n");
     
     return 0;
 }
+
